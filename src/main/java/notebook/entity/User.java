763a3c9de069cf8d241924dd4a549f79fcb7fe3d
Dima Huts <dima_ec_2011@mail.ru>
2018-07-07 14:06:07 +0300

    changed fetch type for user entity

--- src/main/java/notebook/entity/User.java
+++ src/main/java/notebook/entity/User.java
@@ -26,7 +26,7 @@ public class User {
   @Column(name = "active")
   private int active;
 
-  @ManyToMany(cascade = CascadeType.ALL)
+  @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
   private Set<Role> roles;
 
