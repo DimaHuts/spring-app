ad6dd810e500876ddd5518b75badcd43f7cb8058
DimaHuts <dima_ec_2011@mail.ru>
2018-10-20 22:36:15 +0300

    added product category controller

---
+++ src/main/java/notebook/controller/ProductCategoryController.java
@@ -0,0 +1,7 @@
+package notebook.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductCategoryController {
+}
--- src/main/java/notebook/entity/ProductCategory.java
+++ src/main/java/notebook/entity/ProductCategory.java
@@ -12,4 +12,20 @@ public class ProductCategory {
 
     @Column(name = "categoryName")
     private String categoryName;
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
