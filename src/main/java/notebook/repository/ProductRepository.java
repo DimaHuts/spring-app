90e744eb4009639fea289d53f5c988b610a5f01b
dGuts <dmitry.huts@gmail.com>
2018-11-06 13:42:32 +0300

    added entity graph to fetch product

--- src/main/java/notebook/repository/ProductRepository.java
+++ src/main/java/notebook/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package notebook.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import notebook.entity.Product;
@@ -9,12 +10,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+  @EntityGraph(attributePaths = {"categories"})
   @Query("select p from Product p")
   List<Product> findAllProducts();
 
   @Query("select p from Product p where p.userId = :userId")
   List<Product> findProductsByUser(@Param("userId") long userId);
-
-  @Query(value = "insert into product_categories(patient_id, category_id) values (:productId, :categoryIds)", nativeQuery = true)
-  void saveProductCategories(@Param("productId") Long productId, @Param("categoryIds") List<Long> categoryIds);
 }
