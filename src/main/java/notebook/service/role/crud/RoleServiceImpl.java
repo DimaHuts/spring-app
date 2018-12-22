7b6f208aadde72ed379d088078265a2d9861e2c2
dGuts <dmitry.huts@gmail.com>
2018-12-22 12:38:23 +0300

    cleared import

--- src/main/java/notebook/service/role/crud/RoleServiceImpl.java
+++ src/main/java/notebook/service/role/crud/RoleServiceImpl.java
@@ -3,7 +3,6 @@ package notebook.service.role.crud;
 import notebook.entity.Role;
 import notebook.repository.role.RoleRepository;
 import notebook.service.common.BeanProvider;
-import notebook.service.role.crud.RoleService;
 import org.springframework.stereotype.Service;
 
 @Service
