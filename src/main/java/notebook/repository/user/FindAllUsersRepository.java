cd7ab965a2efd4ce30d81aaab764cd7165b4a5b8
dGuts <dmitry.huts@gmail.com>
2018-12-22 11:00:21 +0300

    refactoring users` repository: extend all repository from one

--- src/main/java/notebook/repository/user/FindAllUsersRepository.java
+++ src/main/java/notebook/repository/user/FindAllUsersRepository.java
@@ -2,12 +2,11 @@ package notebook.repository.user;
 
 import notebook.entity.User;
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface FindAllUsersRepository extends JpaRepository<User, Long> {
+public interface FindAllUsersRepository extends UserRepository {
   @Query("select u from User u")
   @EntityGraph(attributePaths = {"permissions"})
   List<User> findAllUsers();
--- src/main/java/notebook/repository/user/FindUserByEmailRepository.java
+++ src/main/java/notebook/repository/user/FindUserByEmailRepository.java
@@ -2,11 +2,10 @@ package notebook.repository.user;
 
 import notebook.entity.User;
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface FindUserByEmailRepository extends JpaRepository<User, Long> {
+public interface FindUserByEmailRepository extends UserRepository {
   @Query("select u from User u where u.email = :email")
   @EntityGraph(attributePaths = {"permissions"})
   User findByEmail(@Param("email") String email);
