2fb04ced11d940f99a36c844fe6499dc337d33f1
dGuts <dmitry.huts@gmail.com>
2019-01-18 12:47:46 +0300

    simplified authentication process

--- src/main/java/notebook/service/authentication/CustomAuthenticationManager.java
+++ src/main/java/notebook/service/authentication/CustomAuthenticationManager.java
@@ -1,22 +1,16 @@
 package notebook.service.authentication;
 
 import notebook.factory.AuthenticationFactory;
-import notebook.service.common.BeanProvider;
 import notebook.service.common.SecurityContextHandler;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomAuthenticationManager implements AuthenticationManagerInterface {
   @Override
   public void authenticate(String username, String password) {
-    AuthenticationManager authenticationManager = BeanProvider.getBean(AuthenticationManager.class);
     Authentication userAuthenticationObject = AuthenticationFactory.getAuthenticationObject(username, password);
 
-    Authentication objectAuthentication = authenticationManager.authenticate(userAuthenticationObject);
-
-    SecurityContextHandler.setAuthentication(objectAuthentication);
+    SecurityContextHandler.setAuthentication(userAuthenticationObject);
   }
 }
