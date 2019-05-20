02e7903cf31754db63e6612a7d3d4f54f29bbef2
dGuts <dmitry.huts@gmail.com>
2019-05-20 13:10:19 +0300

    clean up code

--- src/main/java/notebook/util/jwt/JwtTokenProvider.java
+++ src/main/java/notebook/util/jwt/JwtTokenProvider.java
@@ -3,8 +3,6 @@ package notebook.util.jwt;
 import io.jsonwebtoken.*;
 import notebook.property.JWTProperties;
 import notebook.service.common.BeanProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +10,6 @@ import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
-
-//  private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
 
   public String generateToken(Authentication authentication) {
     String userPrincipal = (String) authentication.getPrincipal();
@@ -46,26 +42,4 @@ public class JwtTokenProvider {
 
     return claims.getSubject();
   }
-
-//  public boolean validateToken(String authToken) {
-//    JWTProperties jwtProperties = BeanProvider.getBean(JWTProperties.class);
-//
-//    String jwtSecret = jwtProperties.getSecret();
-//
-//    try {
-//      Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
-//      return true;
-//    } catch (SignatureException ex) {
-//      logger.error("Invalid JWT signature");
-//    } catch (MalformedJwtException ex) {
-//      logger.error("Invalid JWT token");
-//    } catch (ExpiredJwtException ex) {
-//      logger.error("Expired JWT token");
-//    } catch (UnsupportedJwtException ex) {
-//      logger.error("Unsupported JWT token");
-//    } catch (IllegalArgumentException ex) {
-//      logger.error("JWT claims string is empty.");
-//    }
-//    return false;
-//  }
 }
